0e7cd3cc8098f30a6ad3d2f37e9f8333d270ee64
Arthur Shevchenko <torgadddon@gmail.com>
2012-11-04 23:35:10 +0200

    Добавлен django-debug-toolbar

--- testkiwi/settings.py
+++ testkiwi/settings.py
@@ -1,5 +1,20 @@
 # Django settings for testkiwi project.
 
+INTERNAL_IPS = ('127.0.0.1',)
+
+DEBUG_TOOLBAR_PANELS = (
+    # 'debug_toolbar_user_panel.panels.UserPanel'
+    'debug_toolbar.panels.version.VersionDebugPanel',
+    'debug_toolbar.panels.timer.TimerDebugPanel',
+    'debug_toolbar.panels.settings_vars.SettingsVarsDebugPanel',
+    'debug_toolbar.panels.headers.HeaderDebugPanel',
+    'debug_toolbar.panels.request_vars.RequestVarsDebugPanel',
+    'debug_toolbar.panels.sql.SQLDebugPanel',
+    'debug_toolbar.panels.template.TemplateDebugPanel',
+    'debug_toolbar.panels.signals.SignalDebugPanel',
+    'debug_toolbar.panels.logger.LoggingPanel',
+)
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -98,6 +113,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'testkiwi.urls'
@@ -123,6 +139,8 @@ INSTALLED_APPS = (
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'kiwi',
+    'debug_toolbar',
+    # 'debug_toolbar_user_panel',
 )
 
 # A sample logging configuration. The only tangible logging
